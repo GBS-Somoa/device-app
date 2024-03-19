8894102d0e85b6b94cee1efb5f4e39cf1819f407
이시은 <lsy53430@naver.com>
2024-03-19 12:40:37 +0900

    Chore: 파비콘 소모아 로고로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,21 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "소모아 기기 테스트앱",
-  description: "소모아에서 가전기기의 역할을 하는 테스트 사이트입니다.",
+	title: "소모아 기기 테스트앱",
+	description: "소모아에서 가전기기의 역할을 하는 테스트 사이트입니다.",
+	icons: {
+		icon: "/somoa_logo.png",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
